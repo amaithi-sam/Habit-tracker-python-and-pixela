719c1785933e87f9e1096a43add4975e78dce41d
Amaithi Chirasan <77726326+amaithi-sam@users.noreply.github.com>
2023-01-18 23:21:40 +0530

    Code Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,159 @@
-import requests
-from datetime import datetime
 
-TOKEN = ""
+"""AMAITHI'S HABIT TRACKER USING PIXELA API - FOLLOW THE INSTRUCTIONS TO GET STARTED WITH THIS CODE SNIPPET"""
+
+
+from datetime import datetime
+import requests
+
+
+#------------------- UPDATE THE DETAILES BELOW------------
 
 USERNAME = ""
+TOKEN = ""
+GRAPH_ID = ""
+HABIT_NAME = ""
+GRAPH_UNIT = "Minute"
 
-pixela_endpoint = "https://pixe.la/v1/users"
+#-------------------------Don't Change the Code Below------------
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
-}
-
-#response = requests.post(url=pixela_endpoint, json=user_params)
-#print(response.text)
-
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-graph_id = "graph1"
-graph_config = {
-    "id": graph_id,
-    "name": "Coding Graph",
-    "unit": "Minute",
-    "type": "float",
-    "color": "sora",
-}
-
-headers = {
+HEADERS = {
     "X-USER-TOKEN": TOKEN
 }
 
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(response.text)
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+GRAPH_CONFIG = {
+        "id": GRAPH_ID,
+        "name": HABIT_NAME,
+        "unit": GRAPH_UNIT,
+        "type": "float",
+        "color": "ajisai",
+        "timezone": "Singapore"
+    }
+TODAY = datetime.now().strftime("%Y%m%d")
 
-pixl_endpoint = f"{graph_endpoint}/{graph_id}"
+# ------------------- CREATE NEW USER ------------------- #
 
-today = datetime(day=1, month=11,year=2021)
+def new_user():
 
-pixl_config = {
-    "date": today.strftime("%Y%m%d"),
-    "quantity": "145.50",
-}
+    user_params = {
+        "token": TOKEN,
+        "username": USERNAME,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes",
+    }
 
-# response = requests.post(url=pixl_endpoint, json=pixl_config, headers=headers)
-# print(response.text)
+    pixela_response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+    print(pixela_response.text)
 
-#-----------------PUT - Update a piece of data-------------------
+# ------------------- CREATE NEW GRAPH ------------------- #
 
-put_endpoint = f"{pixl_endpoint}/{today.strftime('%Y%m%d')}"
+def new_graph():
+    
+    graph_response = requests.post(url=GRAPH_ENDPOINT, json=GRAPH_CONFIG, headers=HEADERS)
+    print(graph_response.text)
 
-put_config = {
-    "quantity": "510.65"
-}
-# response = requests.put(url=put_endpoint, json=put_config, headers=headers)
-# print(response.text)
+# ------------------- DELETE GRAPH ------------------- #
 
-#-----------------Delete-----DElete a piece of data-------------------
+def delete_graph():
+    
+    delete_graph_response = requests.delete(
+        url=f"{GRAPH_ENDPOINT}/{GRAPH_ID}",
+        json=GRAPH_CONFIG,
+        headers=HEADERS
+    )
+    print(delete_graph_response.text)
 
-del_endpoint = f"{pixl_endpoint}/{today.strftime('%Y%m%d')}"
+# ------------------- POST A PIXEL ------------------- #
 
-# response = requests.delete(url=del_endpoint, headers=headers)
-# print(response.text)
+def post_a_pixel():
+
+    POST_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+
+    quantity = float(input("How many Minutes did you perform Coding today?\n"))
+
+    POST_PIXEL_CONFIG = {
+        "date": TODAY,
+        "quantity": quantity,
+    }
+
+    post_pixel_response = requests.post(
+        url=POST_PIXEL_ENDPOINT,
+        json=POST_PIXEL_CONFIG,
+        headers=HEADERS
+    )
+    print(post_pixel_response.text)
+
+# ------------------- UPDATE A PIXEL ------------------- #
+
+def update_pixel():
+    
+    date = input("Input date (YYYYMMDD):\n")
+
+    UPDATE_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{date}"
+
+    quantity = float(input(f"How many Minutes did you perform Coding on {date}?\n"))
+
+    UPDATE_PIXEL_CONFIG = {
+        "quantity": quantity,
+    }
+
+    update_pixel_response = requests.put(
+        url=UPDATE_PIXEL_ENDPOINT,
+        json=UPDATE_PIXEL_CONFIG,
+        headers=HEADERS
+    )
+    print(update_pixel_response.text)
+
+# ------------------- DELETE A PIXEL ------------------- #
+
+def del_pixel():
+    
+    delete_date = input("Enter date to delete:\n(yyyyMMdd)\n")
+    DELETE_PIXEL_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{delete_date}"
+
+    delete_pixel_response = requests.delete(url=DELETE_PIXEL_ENDPOINT, headers=HEADERS)
+    print(delete_pixel_response.text)
+    
+    
+#-----------------------    VIEW THE GRAPH-------------
+
+def view_graph():
+    
+    link = f"{GRAPH_ENDPOINT}/{GRAPH_ID}.html"
+    
+    print(f"\n Open the Below Link in a Web Browser To View the Graph..\m\n {link}")
+    
+#------------------------------------------------------------
+    
+if __name__ == "__main__":
+    
+    choice = input("""
+                Would you like to:
+                    1 - Post a pixel
+                    2 - Update a pixel
+                    3 - Delete a pixel
+                    \n
+                    4 - Create a New account
+                    5 - Create a New Graph
+                    6 - View The Graph
+                    7 - Delete a Graph
+                    """)
+    
+    if choice == "1" & choice in range(1,7):
+        post_a_pixel
+    elif choice =="2":
+        update_pixel
+    elif choice == "3":
+        del_pixel
+    elif choice == "4":
+        new_user
+    elif choice == "5":
+        new_graph
+    elif choice == "6":
+        view_graph
+    elif choice == "7":
+        delete_graph
+    else:
+        print("kindly choose Between 1 & 7")
+        choice
